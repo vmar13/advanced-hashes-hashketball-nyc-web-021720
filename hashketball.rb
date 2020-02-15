dca4f3fa4b85acf274b51310b3c1ca8c425e46d6
Vanessa Martinez <vmar76@gmail.com>
2020-02-15 21:24:14 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,13 +1,9 @@
 def game_hash
   hash = {}
-  hash[:home, :away] = {}
-  hash[:team_name, :colors, :players] = {}
+ 
 end
 
 
-# describe '#game_hash' do
-#     let(:top_level_keys) { [:home, :away] }
-#     let(:team_level_keys) { [:team_name, :colors, :players] }
 
 
 
