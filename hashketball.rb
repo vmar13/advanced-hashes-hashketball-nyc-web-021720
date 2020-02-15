0a6d5a1d1288aa23ead470dcacd07b4a9b87d4f0
Vanessa Martinez <vmar76@gmail.com>
2020-02-15 22:41:16 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -116,9 +116,10 @@ def game_hash
 end
 
 def num_points_scored(player_name)
+binding.pry 
   game_hash.each do |location, team_data|
     team_data.each do |attribute, data|
-binding.pry 
+
 end
   
   #return num_points_scored for player_name
