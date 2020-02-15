c79a18ba7beee75c55f78c2023c3f11fbcce87ca
Vanessa Martinez <vmar76@gmail.com>
2020-02-15 21:36:04 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -3,16 +3,18 @@ def game_hash
     :home => {
       :team_name => "Brooklyn Nets",
       :colors => ["Black", "White"],
-      :players => [{
-      :player_name => ?, 
-      :number => ?}, 
-      :shoe => ?}, 
-      :points => ?}, 
-      :rebounds => ?}, 
-      :assists => }, 
-      :steals => ?}, 
-      :blocks => ?}, 
-      :slam_dunks => ?}]
+      :players => [
+        {:player_name => ?, 
+        :number => , 
+        :shoe => , 
+        :points => , 
+        :rebounds => , 
+        :assists => , 
+        :steals => , 
+        :blocks => , 
+        :slam_dunks => 
+    },
+        ]
     }
   }
  
