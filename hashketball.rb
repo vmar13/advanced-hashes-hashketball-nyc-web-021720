7181192b6847c284b36d55678e0a633e9d5449d8
Vanessa Martinez <vmar76@gmail.com>
2020-02-16 00:50:56 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -133,6 +133,20 @@ def find_player(players, player_name)
   end 
 end 
   
+game_hash.each do |location, team_data|
+#     team_data.each do |attribute, data|
+#       if attribute == :players
+#       data.each do |player|
+#         binding.pry 
+#         if player[:player_name] == player_name
+#           return player[:points]
+#         end
+#       end 
+#     end
+#   end
+# end 
+# end  
+  
 def team_colors(team_name)
   game_hash.each do |place, team|
     if team[:team_name] == team_name
