90d8383bccd6b738de0654dffd0b3025c6b4f803
Vanessa Martinez <vmar76@gmail.com>
2020-02-16 04:37:06 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -252,7 +252,8 @@ end
 def long_name_steals_a_ton?
   longest_name = player_with_longest_name
   most_steals = most_steals_player
-binding.pry 
+  binding.pry
+ 
   if longest_name == most_steals 
     return true 
   else 
