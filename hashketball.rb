960a917676c3c603e08a32cb8dfd49548690fcc4
Vanessa Martinez <vmar76@gmail.com>
2020-02-16 04:34:14 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -250,7 +250,7 @@ def player_with_longest_name
 end
 
 def long_name_steals_a_ton?
-  most_steals = 22
+  most_steals = 0
   player_with_most_steals = "Bismack Biyombo"
   game_hash.each do |location, team|
     team.each do |attribute, data|
