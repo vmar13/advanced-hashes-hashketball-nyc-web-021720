b46bbf0691d701196850b293dbbc5a11938aa678
Vanessa Martinez <vmar76@gmail.com>
2020-02-16 03:40:29 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -172,13 +172,14 @@ end
   end 
  
 def player_stats(player_name)
+  new_hash = {}
   game_hash.each do |team, team_data|
     team_data.each do |player_data, value|
         if player_data == :players
           value.each do |player|
             if player_name == player[:player_name]
               player.delete(:player_name)
-              return player
+              new_hash.push(player[:player_name])
             end 
           end 
         end
